dc7c3ea81534fa65fec8f84a5db6e49c63988a1a
CMBCoding <189717925+CMBCoding@users.noreply.github.com>
2025-04-15 10:46:22 -0700

    Finish basic arithemetic functions.

---
+++ script.js
@@ -0,0 +1,19 @@
+//Function for the add button
+const add = function(a, b) {
+    return answer = a + b;
+};
+
+//Function for the subtract button
+const subtract = function(a, b) {
+    return answer = a - b;
+};
+
+//Function for the multiply button
+const multiply = function(a, b) {
+    return answer = a * b;
+};
+
+//Function for the divide button
+const divide = function(a, b) {
+    return answer = a / b;
+};
