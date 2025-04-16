b9ea0ae1d6faeabbb48fa2de5778868610ec97c2
CMBCoding <189717925+CMBCoding@users.noreply.github.com>
2025-04-16 13:32:34 -0700

    Figure out checkOperator and digit value fetches

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ const oneButton = document.querySelector("#one-btn");
 
 //DOM refference to all of the digit buttons
 const digitButtons = document.querySelectorAll(".digit-btn");
+//Variable that counts the new of times the number keys are pressed
+let digitPress = 0;
 
 //DOM refference to all of the operator buttons
 const operatorButtons = document.querySelectorAll(".operator-btn");
@@ -20,7 +22,11 @@ const subtractButton = document.querySelector("#sub-btn");
 const multiplyButton = document.querySelector("#mult-btn");
 const divideButton = document.querySelector("#div-btn");
 const calculateButton = document.querySelector("#calc-btn");
+
+//DOM refference to the clear button
 const clearButton = document.querySelector("#clr-btn");
+
+const display = document.querySelector("#display");
 
 //Function for the add button
 const add = function(a, b) {
@@ -82,18 +88,94 @@ const divide = function(a, b) {
 //Looping through the NodeList created by the query selector selecting all digit buttons, 
 // adding an event to each
 
-/*For each button, add an event listener that assigns an operand the value of the button
+/*For each digit button, add an event listener that assigns an operand the value of the button
 (as assigned in the HTML and fetched via the DOM) */
+
+const getOperand = function() {
+
+}
+
 digitButtons.forEach(button => {
-    button.addEventListener('click', (event) => {
-        const operand = button.value;
-        console.log(`You press the number ${operand} button`);
-        return operand;
+button.addEventListener("click", (event) => {
+    const operand = button.value;
+    display.innerText = button.value;
+    console.log(`You pressed the number ${operand} button`);
+    digitPress++;
+    if (digitPress === 1) {
+        const numberA = button.value;
+        console.log(`Number A is ${numberA}`)
+        operate(numberA);
+    } else if (digitPress > 1) {
+        const numberB = button.value;
+        console.log(`Number B is ${numberB}`);
+        operate(numberB);
+    } 
+    // else if (digitPress > 2) {
+    //     resetDigitPress;
+    // }
+    console.log(`The number of digit presses is ${digitPress}`)
     });
 });
 
+// function resetDigitPress() {
+//     digitPress = 0;
+// };
+
+/*For each operation button, add an event listener that assigns and returns and operator value 
+based on which button is pressed */
+operatorButtons.forEach(button => {
+    button.addEventListener("click", (event) => {
+        const operatorText = button.innerText;
+        const operatorType = button.id;
+        console.log(`You pressed the ${operatorText} button (${operatorType})`);
+        checkOperator(operatorType);
+        // operate(operator);
+    });
+});
+
+//Give the clear button functionality
+clearButton.addEventListener("click", () => {
+    display.innerText =  "";
+})
+
+//Give the = button functionality, clicking it runs the operate function
+calculateButton.addEventListener("click", () => {
+
+})
+
 //Function for running the calculator operation
-const operate = function(num1, num2) {
+
+const operate = function(numberA, numberB, equation) {
+    console.log(`${numberA} has been recieved`);
+    console.log(`${numberB} has been recieved`)
+    // if (!numberA) {
+    //     operand = numberA;
+    //     console.log(`The first operand is ${numberA}`);
+    // } else if (numberA) {
+    //     operand = numberB;
+    //     console.log(`The second operand is ${numberB}`);
+    // }
+    // console.log(`The first operand is ${operand}`);
+
+    // equation = checkOperator();
+    // console.log(`${equation}`)
+}
+
+//Function that checks the operator type from the operator button pressed and passes it on to operate
+const checkOperator = function(buttonValue) {
+    switch (buttonValue) {
+        case "add-btn" : console.log(add(2,2));
+        break;
+        case "sub-btn" : console.log(subtract(10,2));
+        break;
+        case "mult-btn" : console.log(multiply(5,5));
+        break;
+        case "div-btn" : console.log(divide (40, 2));
+        break;
+    }
+};
+
+/*const operate = function(num1, num2) {
    let a = num1;    
    let b = num2;    
    let answer;
@@ -110,4 +192,4 @@ const operate = function(num1, num2) {
         divide(a,b);
    };
    return answer;
-}
+}*/
