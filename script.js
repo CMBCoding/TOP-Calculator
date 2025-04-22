b30304ac4ed6735a11cb542553980fee1671c8cb
CMBCoding <189717925+CMBCoding@users.noreply.github.com>
2025-04-22 11:20:15 -0700

    Rework code in script to use object value/keys instead of strings

--- script.js
+++ script.js
@@ -18,11 +18,15 @@ const allButtons = document.querySelectorAll(".btn");
 //Variable that counts the new of times the number keys are pressed
 let digitPress = 0;
 
+//Object for holding the operand 1, operator, and operand 2
+const equationObj = {
+    numberA : [],
+    operator : undefined,
+    numberB : [],
+};
+
 /*Variables for holding the two operands, the operator for use in the operate() function, 
 and the operator count for use is evaluations > a pair */
-let numberA = "";
-let numberB = "";
-let operatorType = undefined;
 let operatorCount = 0;
 
 //DOM refference to all of the operator buttons
@@ -63,22 +67,23 @@ const divide = function(a, b) {
 (as assigned in the HTML and fetched via the DOM)*/
 digitButtons.forEach(button => {
 button.addEventListener("click", (event) => {
-    const operand = button.value;
+    const operand = button.value
+    const operatorType = equationObj.operator;
     display.innerText += button.value;
     console.log(`You pressed the number ${operand} button`);
+    
     //If there is not an operatorType, numberA will continue to grow with each digit button press.
-
     if (!operatorType) {
-        numberA += operand;
+        equationObj.numberA += operand;
         console.log(`Operator count is ${operatorCount}`)
-        console.log(`Number A is ${numberA}`)
+        console.log(`Number A is ${equationObj.numberA}`)
         return;
     } 
 
     //If there is an operatorType, numberB will continue to grow until calculate is clicked.
-    if (operatorType && numberA) {
-        numberB += operand;
-        console.log(`Number B is ${numberB}`);
+    if (operatorType && equationObj.numberA) {
+        equationObj.numberB += operand;
+        console.log(`Number B is ${equationObj.numberB}`);
         return;
     }
 })});
@@ -89,13 +94,14 @@ operatorButtons.forEach(button => {
     button.addEventListener("click", (event) => {
         const operatorText = button.innerText;
         const newOperatorType = button.id;
+        equationObj.operator = button.id;
         operatorCount += 1;
         console.log(`You pressed the ${operatorText} button (${newOperatorType})`);
         
 /*If there is more than one operator button press and already numberA and B, operate is called and ran
 allowing for only a pair of equations to be run at once */
-        if (operatorCount > 1 && numberA && numberB) {
-            let result = operate(numberA, numberB, operatorType);
+        if (operatorCount > 1 && equationObj.numberA && equationObj.numberB) {
+            let result = operate(equation);
             display.innerText = result + operatorText;
             console.log(`Pair equation executed, the returned values is ${numberA}`);
             result = numberA;
@@ -119,10 +125,10 @@ clearButton.addEventListener("click", () => {
     operatorCount = 0;
 });
 
-/*Give the = button functionality. Clicking it gets the operands, 
-operatorType and runs the operate function*/
+/*Give the = button functionality. Clicking it sends the object containing operands and
+operator to the operate function*/
 calculateButton.addEventListener("click", () => {
-    result = operate(numberA, numberB, operatorType);
+    result = operate(equationObj);
 });
 
 /*Give the +/- button functionality. Clicking it changes the current operand to 
@@ -136,20 +142,18 @@ deleteButton.addEventListener("click", () => {
 });
 
 //Function for running the calculator operation
-/*Takes the inputs and operator sent from the calculate button, 
+/*Takes the inputs and operator sent from the calculate button, extracts them from the object,
 converts the inputs to numbers, and operates*/
-const operate = function(numberA, numberB, operatorType) {
-    console.log(`${numberA} has been recieved`);
-    console.log(`${numberB} has been recieved`);
-    console.log(`${operatorType} has been recieved`);
-
-    let operandA = Number(numberA);
-    let operandB = Number(numberB);
-
+const operate = function(equationObj) {
+    let operandA = Number(equationObj.numberA);
+    let operandB = Number(equationObj.numberB);
+    let operatorType = equationObj.operator;
+    
     let result;
     console.log(`Operand A is ${operandA}`);
     console.log(`Operand B is ${operandB}`);
-    
+    console.log(`operatorType is ${operatorType}`); 
+
     switch (operatorType) {
         case "add-btn" : console.log(`Add`);
             result = add(operandA, operandB);
